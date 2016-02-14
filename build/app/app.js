85817455f6001df0a4994edd430b504480e12490
Griffin Michl <griffinmichl@gmail.com>
2016-02-13 23:12:06 -0800

    implement transition time tracking

--- build/app/app.js
+++ build/app/app.js
@@ -6,10 +6,28 @@ angular.module('profiler', ['profiler.services']).controller('ProfileController'
   });
 
   $scope.selected = { id: 0 };
-  $scope.press = function (event) {
-    var key = String.fromCharCode(event.charCode);
+
+  var data = {};
+  var key = '';
+  var timeDown = undefined;
+  var timeUp = undefined;
+  var down = false;
+  // TODO: consider/test for edge cases
+  $scope.handleDown = function (event) {
+    if (!down) {
+      key = String.fromCharCode(event.which);
+      timeDown = event.timeStamp;
+      down = true;
+    }
+  };
+
+  $scope.handleUp = function (event) {
+    timeUp = event.timeStamp;
+    data[key] ? data[key].push(timeUp - timeDown) : data[key] = [timeUp - timeDown];
     if (key === ' ') {
       $scope.selected.id++;
     }
+    down = false;
+    console.log(data);
   };
 });
--- client/app/app.js
+++ client/app/app.js
@@ -7,12 +7,44 @@ angular.module('profiler', [
     });
 
     $scope.selected = {id: 0};
-    $scope.press = (event) => {
-      let key = String.fromCharCode(event.charCode);
+
+    let pressTimes = {};
+    let transition_time = {};
+    let lastKey = '';
+    let key = '';
+    let timeDown;
+    let timeUp;
+    let down = false;
+
+    // TODO: consider/test for edge cases
+    $scope.handleDown = (event) => {
+      if (!down) {
+        key = String.fromCharCode(event.which);
+        timeDown = event.timeStamp;
+
+        let transitionTime = timeDown - timeUp;
+        if (lastKey in transition_time) {
+          if (key in transition_time[lastKey]) {
+            transition_time[lastKey][key].push(transitionTime);
+          } else { // may be unnecessary
+            transition_time[lastKey][key] = [transitionTime];
+        } else {
+          transition_time[lastKey] = {key: [transitionTime]};
+        }
+
+
+        down = true;
+      }
+    };
+
+    $scope.handleUp = (event) => {
+      timeUp = event.timeStamp
+      pressTimes[key] ? pressTimes[key].push(timeUp - timeDown) : pressTimes[key] = [timeUp - timeDown];
       if (key === ' ') {
         $scope.selected.id++;
       }
-    }
-
+      down = false;
+      lastKey = key;
+    };
 
   });
